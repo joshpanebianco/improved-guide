483815358332daef3da50b9a5d4575944be963f2
Josh <pana23@gmail.com>
2020-04-03 19:52:55 +1100

    installed bootstrap and added the import to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import SignUp from './SignUp';
 import Home from './Home';
 import Survey from './Survey';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
@@ -18,3 +19,7 @@ function App() {
 }
 
 export default App;
+
+
+// Notes from Alex
+// import {Card} from "react-bootstrap"
