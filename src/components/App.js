10e5aaf596b69212614f6c54b925fee6c9f581ce
Josh <pana23@gmail.com>
2020-04-04 16:02:55 +1100

    fixed bugs with bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -18,37 +18,36 @@ import {
   NavDropdown,
   Form,
   FormControl,
-  Button
+  Button,
+  Container
 } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar bg="primary" variant="dark" expand="lg" className="mb-4">
+          <Container>
+            <Navbar.Brand href="#home">Project Two</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
               </Nav>
-              <Form inline>
-                // <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                // <Button variant="outline-success">Search</Button>
-              </Form>
+
+                <Button className="mr-3" variant="dark">login</Button>
+                <Button variant="success">SignUp</Button>
+
             </Navbar.Collapse>
+            </Container>
       </Navbar>
-      <LogIn />
-      <SignUp />
-      <Home />
-      <Survey />
+      <Container>
+        <LogIn />
+        <SignUp />
+        <Home />
+        <Survey />
+      </Container>
+
     </div>
   );
 }
