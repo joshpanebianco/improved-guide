00bd0e86f56a43aaeee164cb7199488a67d85ef9
Josh <pana23@gmail.com>
2020-04-05 19:42:55 +1000

    small fixes to the forms

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -80,7 +80,7 @@ _handleSubmit(event) {
 
          <Form.Group className="w-50">
             <Form.Label>Last Name</Form.Label>
-            <Form.Control name="firstname" type="text" placeholder="First name" value={ this.state.firstname } onChange={ this._handleChange } autoFocus required />
+            <Form.Control name="lasttname" type="text" placeholder="Last name" value={ this.state.firstname } onChange={ this._handleChange } autoFocus required />
          </Form.Group>
 
           <Form.Group className="w-50">
