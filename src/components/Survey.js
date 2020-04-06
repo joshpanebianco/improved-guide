149006482f0df6d5aef137c691c0417d0b36c6fe
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-04-07 07:30:37 +1000

    Added histories and updated with seen

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const GALLERY_URL = 'https://localhost:3000/requests/galleries/2.json'
-
 import {
   Navbar,
   Nav,
@@ -18,80 +16,75 @@ import {
 } from "react-bootstrap";
 
 class Survey extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      isFetching: false,
       countSeen: 0,
       countUnSeen: 0,
-      allAds: [
-        {id: 1, image: 'https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/1/9/0/3/2903091-5-eng-GB/Extending-equity-and-iconic-appeal-Coca-Cola-unites-brands-with-global-campaign_wrbm_medium.jpg', seen: false},
-        {id: 2, image: 'https://s3-ap-southeast-1.amazonaws.com/images.marketing-interactive.com/wp-content/uploads/2017/06/no-sugar-e1497234889946.jpg', seen: false},
-        {id: 3, image: 'https://www.communicus.com/wp-content/uploads/2017/03/coca-cola-ad-drink.jpg', seen: false}
-      ],
+      allAds: [],
+      allHistories: [],
+      galleryId: props.match.params.galleryId,
       gallery: {},
     }
 
     this.saveCount = this.saveCount.bind(this);
     this.updateSeen = this.updateSeen.bind(this);
 
-
-    // const request = require('request');
-    // request('https://localhost:3000/requests/galleries/2.json', function (error, response, body) {
-    //   console.error('error:', error); // Print the error if one occurred
-    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //   console.log('body:', body); // Print the HTML for the Google homepage.
-    // });
-    //
-    // // At request level
-    // const https = require('https');
-    // const agent = new https.Agent({
-    //   rejectUnauthorized: false
-    // });
-    //
-    // axios.get('https://localhost:3000/requests/galleries/2.json', { httpsAgent: agent })
-
-    // const galleryList = axios.create({
-    //   withCredentials: true,
-    //   headers: {
-    //     'Accept': 'application/json',
-    //   }
-    // });
-
-    const fetchGallery = () => {
-      axios.get('http://localhost:3000/requests/galleries/2.json', {withCredentials: false}).then(results => {
-        this.setState({gallery: results.data.gallery});
-        console.log(results.data.gallery);
-      });
-    };
-
-    fetchGallery();
   }
 
-updateSeen(index) {
-  index -= 1;
-  const ads = this.state.allAds;
-  ads[index].seen = true;
-  this.setState({allAds: ads });
-}
+  fetchGallery = () => {
+    const SERVER_URL = 'http://localhost:3000/requests/galleries/'+ this.state.galleryId +'.json';
+    this.setState({...this.state, isFetching: true});
+    axios.get(SERVER_URL, {withCredentials: false}).then(results => {
+      this.setState({gallery: results.data.gallery});
+      this.setState({allAds: results.data.ads});
+      this.setState({allHistories: results.data.histories})
+      this.setState({...this.state, isFetching: false});
+    });
+  };
 
-
-saveCount(content) {
-  if (content === true) {
-    this.setState({countSeen: this.state.countSeen + 1});
-
-  } else {
-    this.setState({countUnSeen: this.state.countUnSeen + 1});
+  // Fetch gallery on page load
+  componentDidMount() {
+    this.fetchGallery()
   }
 
-};
+  // Update history 'has_seen' with user input
+  updateSeen(index, boolean) {
+    const allHistories = this.state.allHistories;
+    allHistories[index - 1].has_been_seen = boolean;
+    this.setState({allHistories: allHistories});
+    console.log(allHistories);
+
+    // index -= 1;
+    // const ads = this.state.allAds;
+    // ads[index].seen = true;
+    // this.setState({allAds: ads });
+  }
+
+  saveCount(content) {
+    if (content === true) {
+      this.setState({countSeen: this.state.countSeen + 1});
+
+    } else {
+      this.setState({countUnSeen: this.state.countUnSeen + 1});
+    }
+
+  };
 
   render() {
-    return (
-      <div>
-        <Gallery images={ this.state.allAds } increasePoint={ this.saveCount } updateSeen={this.updateSeen} />
-        <Score counter={ this.state.countSeen}/>
-      </div>
-    );
+      const isFetching = this.state.isFetching;
+      return (
+            <div>
+              {isFetching
+                ? <p>Loading</p>
+                : <div>
+                  <Gallery gallery={this.state.gallery} images={ this.state.allAds } increasePoint={ this.saveCount } updateSeen={this.updateSeen} />
+                  <Score counter={ this.state.countSeen}/>
+                </div>
+              }
+            </div>
+      );
   }
 };
 
@@ -103,7 +96,7 @@ class Gallery extends Component {
     this.state = {
       index: 0,
       add: this.props.images[0],
-
+      gallery: this.props.gallery,
     }
     this._onClickSeen = this._onClickSeen.bind(this);
     this._onClickUnSeen = this._onClickUnSeen.bind(this);
@@ -114,14 +107,14 @@ class Gallery extends Component {
   _onClickSeen(event) {
       const index = this.state.index + 1;
       this.setState({index: index, add: this.props.images[index]});
-      this.props.updateSeen(index);
+      this.props.updateSeen(index, true);
       this.props.increasePoint(true);
   };
 
   _onClickUnSeen(event) {
-    // this.setState({seen: false});
     const index = this.state.index + 1;
     this.setState({index: index, add: this.props.images[index]});
+    this.props.updateSeen(index, false);
     this.props.increasePoint(false);
   };
 
@@ -134,9 +127,9 @@ class Gallery extends Component {
         <div>
           <Row className="d-flex justify-content-center">
             <Card className="shadow">
-              <Card.Header as="h5" className="text-white bg-dark">Company Name</Card.Header>
+              <Card.Header as="h5" className="text-white bg-dark">{this.state.gallery.name}</Card.Header>
               <Card.Body>
-                <Card.Title>Campaign Title</Card.Title>
+                <Card.Title>{this.state.gallery.name}</Card.Title>
                 <Card.Text>
                   <img className="survey-img" src={this.state.add.image}/>
                 </Card.Text>
