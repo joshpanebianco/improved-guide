091a8dacf01e057483bdddc28fcfa9cc81a07953
sam nejat <samnejat@sams-mbp.home>
2020-04-07 15:34:41 +1000

    Home page linking to survey

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import {
   Navbar,
@@ -13,34 +15,62 @@ import {
 } from "react-bootstrap";
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isFetching: false,
+      galleries: [],
+
+    }
+  }
+
+  fetchAds = () => {
+    const SERVER_URL = 'http://localhost:3000/requests/galleries.json';
+    this.setState({...this.state, isFetching: true});
+    axios.get(SERVER_URL, {withCredentials: false}).then(results => {
+      this.setState({galleries: results.data.galleries});
+      this.setState({...this.state, isFetching: false});
+    })
+  }
+
+  // Fetch ads on page load
+  componentDidMount() {
+    this.fetchAds()
+  }
+
+
   render() {
     return (
       <div>
-        <Card className="w-75 mb-4">
-          <Card.Header as="h5" className="text-white bg-dark">Company Name</Card.Header>
-          <Card.Body>
-            <Card.Title>Title of the campaign</Card.Title>
-            <Card.Text>
-              With supporting text below as a natural lead-in to additional content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-         </Card>
-
-         <Card className="w-75">
-           <Card.Header as="h5" className="text-white bg-dark">Company Name</Card.Header>
-           <Card.Body>
-             <Card.Title>Title of the campaign</Card.Title>
-             <Card.Text>
-               With supporting text below as a natural lead-in to additional content.
-             </Card.Text>
-             <Button variant="primary">Go somewhere</Button>
-           </Card.Body>
-          </Card>
+      {this.state.galleries.map ((gallery) => {
+        return (<Gallery gallery={gallery} key={gallery.id} />)
+      })}
 
       </div>
     );
   }
 }
+
+
+class Gallery extends Component {
+
+  render() {
+    return (
+
+      <Card className="w-75 mb-4">
+
+        <Card.Header as="h5" className="text-white bg-dark">{this.props.gallery.name}</Card.Header>
+        <Card.Body>
+          <Card.Title>{this.props.gallery.type}</Card.Title>
+          <Link to={`/survey/${this.props.gallery.id}`}>
+            <Button variant="primary">start</Button>
+          </Link>
+        </Card.Body>
+       </Card>
+
+    );
+  }
+}
+
 
 export default Home;
