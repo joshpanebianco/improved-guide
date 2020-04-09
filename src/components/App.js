910209d1248fbdd8eb29a02e4f55cb4f810c7b46
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-04-09 16:26:13 +1000

    Amended logged in False

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isLoggedIn: true,
+      isLoggedIn: false,
       user: {}
     };
   }
@@ -56,7 +56,7 @@ class App extends Component {
 
   handleLogout = () => {
     this.setState({
-      isLoggedIn: true,
+      isLoggedIn: false,
       user: {}
     })
   }
